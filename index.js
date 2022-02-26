e06a94add65a3179afc4b10271e5c00041879f97
ooreally <anu1512sri@gmail.com>
2022-02-27 01:20:35 +0530

    fix: fixed the links  the links in side-nav

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*index.js code for mobile view of decora*/
 const hamburger = document.querySelector("#head-ham");
 const hamContent = document.querySelector("#head-ham-content");
 
@@ -8,3 +9,17 @@ const hamClicked = () => {
     hamContent.style.display = "flex";
 }
 hamburger.addEventListener("click", hamClicked )
+
+
+/*js code for navigation component to illustrate navigation bar*/ 
+const navHamburger = document.querySelector("#hamburger-drop");
+const navHamContent = document.querySelector("#ham-content");
+
+const navHamClicked = () => {
+    if(navHamContent.style.display === "flex")
+    navHamContent.style.display = "none";
+    else
+    navHamContent.style.display = "flex";
+}
+
+navHamburger.addEventListener("click", navHamClicked );
