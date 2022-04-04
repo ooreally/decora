0dc5342f763e4797860d475635cca71a567e0146
ooreally <anu1512sri@gmail.com>
2022-04-05 00:15:05 +0530

    feat: added component modal

--- index.js
+++ index.js
@@ -45,3 +45,22 @@ toastBtn.addEventListener("click", toastClickedHandler);
 
 if(closeToast)
 closeToast.addEventListener("click", closeToastClickedHandler);
+
+//jscode for Modal Component
+const modalBtn = document.querySelector("#see-modal-btn");
+const modal = document.querySelector("#modal");
+const closemodal = document.querySelector("#close-modal-btn");
+
+const modalClickedHandler = () => {
+  modal.style.visibility = "visible";
+};
+
+const closeModalClickedHandler = () => {
+    modal.style.visibility="hidden";
+}
+
+if(modalBtn)
+modalBtn.addEventListener("click", modalClickedHandler);
+
+if(closemodal)
+closemodal.addEventListener("click", closeModalClickedHandler);
